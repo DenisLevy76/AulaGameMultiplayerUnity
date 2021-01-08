1746bc229d08f8bdf44e9b5c156e441df99fcd0e
Denis Levy <denis.levi11@gmail.com>
2021-01-08 20:16:32 -0300

    Create and join room

---
+++ Assets/Scripts/GestorDeRede.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using System;
+
+public class GestorDeRede : MonoBehaviourPunCallbacks
+{
+    public static GestorDeRede Instancia { get; private set; }
+    private void Awake() {
+        if (Instancia != null && Instancia != this){
+            gameObject.SetActive(false);
+            return;
+        }
+        Instancia = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+  public void CriaSala(string nomeSala)
+  {
+    PhotonNetwork.CreateRoom(nomeSala);
+  }
+  public void EntrarSala(string nomeSala)
+  {
+    PhotonNetwork.JoinRoom(nomeSala);
+  }
+  public void MudaNick(string nickname){
+      PhotonNetwork.NickName = nickname;
+  }
+
+  private void Start(){
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conexão bem sucedida");
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Menu : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private MenuEntrada menuEntrada;
+    private void Start() {
+        menuEntrada.gameObject.SetActive(false);
+    }
+  public override void OnConnectedToMaster()
+  {
+        menuEntrada.gameObject.SetActive(true);
+  }
+}
